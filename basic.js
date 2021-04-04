8f21298372d59e5b7cb8fa5b936482bbe4e7bddb
Mariya-Sudyarova <kopylova.masha@gmail.com>
2021-04-04 15:37:58 +0300

    my first commit

---
+++ basic.js
@@ -0,0 +1,24 @@
+let userFirstName = 'alex'; // camel case style
+let user_first_name; // snake case style
+
+//------------------------------------------
+//практичесое задание
+
+let date_of_space_event = '12 апреля 1961';
+let name_of_spaceship = '"Восток"';
+let pilot_name = 'ГАГАРИН Юрий Алексеевич';
+let pilot_nation = 'Союза Советских Социалистических Республик';
+let pilot_status = 'гражданин';
+let pilot_rank = 'летчик майор';
+
+//-----------------------------------------
+//Типы данных. Строка-String
+
+const userName = 'Alex';
+const user_Age = '28';
+
+const sayHello = `I am ${userName}, I am ${user_Age}`; // современный вариант с обратными кавычками.
+
+// const sayHello = 'I am ' + userName + ', I am ' + user_Age; // устаревший вариант с одинарными или двойными кавычками
+
+alert(sayHello); 
